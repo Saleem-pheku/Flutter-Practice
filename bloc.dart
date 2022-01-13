9b4f07744219b77323c3ca41fd10124fa3c5ed8d
Moin <76849575+Moinuddin9777@users.noreply.github.com>
2022-01-13 23:14:22 +0530

    Update bloc.dart

--- bloc.dart
+++ bloc.dart
@@ -40,3 +40,25 @@ class App extends StatelessWidget {
     );
   }
 }
+/// {@template app_view}
+/// A [StatelessWidget] that:
+/// * reacts to state changes in the [ThemeCubit]
+/// and updates the theme of the [MaterialApp].
+/// * renders the [CounterPage].
+/// {@endtemplate}
+class AppView extends StatelessWidget {
+  /// {@macro app_view}
+  const AppView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeCubit, ThemeData>(
+      builder: (_, theme) {
+        return MaterialApp(
+          theme: theme,
+          home: const CounterPage(),
+        );
+      },
+    );
+  }
+}
