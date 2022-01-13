4a0820b3c76c868fc42795431139e63a50ba0fdd
Moin <76849575+Moinuddin9777@users.noreply.github.com>
2022-01-13 23:12:49 +0530

    Update bloc.dart

--- bloc.dart
+++ bloc.dart
@@ -22,3 +22,21 @@ class AppBlocObserver extends BlocObserver {
     print(transition);
   }
 }
+/// {@template app}
+/// A [StatelessWidget] that:
+/// * uses [bloc](https://pub.dev/packages/bloc) and
+/// [flutter_bloc](https://pub.dev/packages/flutter_bloc)
+/// to manage the state of a counter and the app theme.
+/// {@endtemplate}
+class App extends StatelessWidget {
+  /// {@macro app}
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => ThemeCubit(),
+      child: const AppView(),
+    );
+  }
+}
