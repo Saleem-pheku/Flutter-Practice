88ee03be9cf518200ea14d9ae74d43f48b3ff2d3
Moin <76849575+Moinuddin9777@users.noreply.github.com>
2022-01-25 02:42:25 +0530

    Create 20_Jan_latest.dart

---
+++ 20_Jan_latest.dart
@@ -0,0 +1,63 @@
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Home()
+));
+
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'The  Moin App ',
+          style: GoogleFonts.abrilFatface(textStyle: TextStyle(color: Colors.white)
+          ),
+        ),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+      ),
+      body: Row(
+        children: [
+          Expanded(
+              child: Image.asset('assets/moin.jpg'),
+            flex: 1,
+
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(23),
+              color: Colors.tealAccent,
+              child: Text('1'),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+            padding: EdgeInsets.all(23),
+            color: Colors.amberAccent,
+            child: Text('2'),
+          ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+            padding: EdgeInsets.all(23),
+            color: Colors.lightBlue,
+            child: Text('3'),
+          ),
+          ),
+
+
+        ],
+      ),
+      );
+  }
+}
+
